ae2fe986f890c51ac081550e82d3b8b5808dc6a8
Nayunn <antoine.toffano@gmail.com>
2022-11-10 14:13:23 +0100

    gzip sizing fixed : headers removed

--- profiling.py
+++ profiling.py
@@ -32,6 +32,13 @@ def monitor(func):
     return logs
 
 def monitor_gzip(file):
+    # data\ecoli_100Kb_reads_10x.fasta_comp 1020000
+    # data\ecoli_100Kb_reads_120x.fasta_comp 12240000
+    # data\ecoli_100Kb_reads_20x.fasta_comp 2040000
+    # data\ecoli_100Kb_reads_40x.fasta_comp 4080000
+    # data\ecoli_100Kb_reads_5x.fasta_comp 510000
+    # data\ecoli_100Kb_reads_80x.fasta_comp 8160000
+    unsorted_comp_size =  {"data/ecoli_100Kb_reads_10x.fasta" : 1020000, "data/ecoli_100Kb_reads_120x.fasta" : 12240000, "data/ecoli_100Kb_reads_20x.fasta" : 2040000, "data/ecoli_100Kb_reads_40x.fasta" : 4080000, "data/ecoli_100Kb_reads_5x.fasta" : 510000, "data/ecoli_100Kb_reads_80x.fasta" : 8160000}
     logs = {}
     worker_process = subprocess.Popen(["gzip", "-f", "-k", file])
     p = psutil.Process(worker_process.pid)
@@ -53,7 +60,7 @@ def monitor_gzip(file):
         except:
             pass
     logs['exec_time'] = time.time() - p.create_time()
-    logs['compression_ratio'] = os.path.getsize(file) / os.path.getsize(file + ".gz")
+    logs['compression_ratio'] = unsorted_comp_size[file] / os.path.getsize(file.replace('data/', 'data/ori/') + ".gz")
     worker_process.wait()
     os.remove(f'{file}.gz')
 
