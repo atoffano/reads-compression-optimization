3b45d1f28d4923e2b3222fccdf722db86eca0869
atoffano <antoine.toffano@gmail.com>
2022-12-05 13:33:19 +0100

    merge with revamped monitoring

--- kmer_sort.py
+++ kmer_sort.py
@@ -6,7 +6,7 @@ from argparse import ArgumentParser
 from typing import Generator
 import subprocess
 from utils import fasta_reader, get_sequence, gzip_out, open_wipe_and_add
-from profiling import monitor, monitor_gzip
+from utils import *
 
 
 def get_next_kmer(method: str, size: int, input: Path):
