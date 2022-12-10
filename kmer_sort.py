edc3297aa24a79e6f4909f02fe0653406d534456
jules_g <jules.garreau00@gmail.com>
2022-12-10 20:16:07 +0100

    lets get in trouble...

--- kmer_sort.py
+++ kmer_sort.py
@@ -1,166 +1,244 @@
 import random
-import operator
-import subprocess
+from copy import deepcopy
+
+# import operator
 from pathlib import Path
-from argparse import ArgumentParser
 from typing import Generator
-import subprocess
 from utils import *
 
 
-def get_next_kmer(method: str, size: int, input: Path):
+# def get_next_kmer(method: str, size: int, infile: Path) -> str:
 
-    if method == "random":
-        return get_random_kmer(size=size, input=input)
+#     if method == "random":
+#         return get_random_kmer(size=size, infile=infile)
 
-    elif method == "occurence-max":
-        return get_occ_kmer(method="max", input=input, size=size)
+#     elif method == "occurence-max":
+#         return get_occ_kmer(method="max", infile=infile, size=size)
 
-    elif method == "occurence-rate":
-        return get_occ_kmer(method="rate", input=input, size=size)
+#     elif method == "occurence-rate":
+#         return get_occ_kmer(method="rate", infile=infile, size=size)
 
-    elif method == "first-found":
-        return get_first_kmer(input=input, size=size)
+#     elif method == "first-found":
+#         return get_first_kmer(infile=infile, size=size)
 
-    else:
-        raise "Method do not exist"
-
-
-def get_first_kmer(input, size: int):
-    return get_sequence(next(fasta_reader(input)))[0:size]
+#     else:
+#         raise "Method do not exist"
 
 
-def get_random_kmer(size: str, input: Path) -> str:
+# def get_first_kmer(infile: str, size: int) -> str:
+#     return get_sequence(next(fasta_reader(infile)))[0:size]
 
-    seq: str = get_sequence(next(fasta_reader(input)))
+
+# def get_occ_kmer(method: str, infile: Path, size: int) -> str:
+
+#     reader: Generator = fasta_reader(filename=infile)
+#     kmers_score: dict = {}
+#     for read in reader:
+#         kmer_found_in_seq = False
+#         seq = get_sequence(read)
+#         for i in range(len(seq) - size):
+#             kmer = seq[i : i + size]
+
+#             if not kmer in kmers_score:
+#                 kmers_score[kmer] = [1, 1]
+#                 kmer_found_in_seq = True
+#             else:
+#                 kmers_score[kmer][0] += 1
+#                 # kmers_score[kmer] += 1
+#                 if not kmer_found_in_seq:
+#                     kmers_score[kmer][1] += 1
+
+#     if method == "rate":
+#         for key in kmers_score:
+#             kmers_score[key] = kmers_score[key][0] / kmers_score[key][1]
+
+#     elif method == "max":
+#         for key in kmers_score:
+#             kmers_score[key] = kmers_score[key][0]
+
+#     return max(kmers_score.items(), key=operator.itemgetter(1))[0]
+# def split_in_chunks(infile: Path, split_size: int) -> list:
+
+#     line_no = len([1 for line in fasta_reader(infile) if line]) * 2
+#     if line_no > 240000 or split_size == 0:
+#         chunk_files = []
+#         sep = line_no // 240000
+#         chunk_sizes = [int(line_no / sep) for _ in range(sep)]
+#         if line_no % 240000 != 0:
+#             chunk_sizes.append(line_no % 240000)
+
+#         reader = fasta_reader(infile)
+#         for i, chunksize in enumerate(chunk_sizes):
+#             chunk_name = f"tempfile/kmer_sort_c{i}_temp"
+#             chunk_files.append(chunk_name)
+#             with open_wipe_and_add(chunk_name) as chunk:
+#                 for _ in range(chunksize):
+#                     try:
+#                         chunk.write(next(reader) + "\n")
+#                     except StopIteration:
+#                         pass
+
+#         return chunk_files
+
+#     else:
+#         return [infile]
+
+
+def get_random_kmer(size: str, infile: Path) -> str:
+
+    seq: str = get_sequence(next(fasta_reader(infile)))
     start: int = random.randint(0, len(seq) - size)
     return seq[start : start + size]
 
 
-def get_occ_kmer(method: str, input: Path, size: int) -> str:
+def get_identifier(read: str, kmer: str, intervals: list) -> str:
+    seq = get_sequence(read)
+    kmer_positions = ""
+    last_kmer_index = 0
+    move_index = 1
+    while move_index != 0:  # if the result of the find() is -1 we stop the loop
 
-    reader: Generator = fasta_reader(filename=input)
-    kmers_score: dict = {}
-    for read in reader:
-        kmer_found_in_seq = False
-        seq = get_sequence(read)
-        for i in range(len(seq) - size):
-            kmer = seq[i : i + size]
+        move_index = seq[last_kmer_index:].find(kmer) + 1
 
-            if not kmer in kmers_score:
-                kmers_score[kmer] = [1, 1]
-                kmer_found_in_seq = True
+        # a = input()
+        if move_index > 0:
+            last_kmer_index += move_index
+
+            if last_kmer_index >= intervals[-1]:
+                kmer_positions += str(len(intervals))
             else:
-                kmers_score[kmer][0] += 1
-                # kmers_score[kmer] += 1
-                if not kmer_found_in_seq:
-                    kmers_score[kmer][1] += 1
+                kmer_positions += str(
+                    [j for j, int in enumerate(intervals) if last_kmer_index > int][-1]
+                    + 1
+                )
 
-    if method == "rate":
-        for key in kmers_score:
-            kmers_score[key] = kmers_score[key][0] / kmers_score[key][1]
-
-    elif method == "max":
-        for key in kmers_score:
-            kmers_score[key] = kmers_score[key][0]
-
-    return max(kmers_score.items(), key=operator.itemgetter(1))[0]
+    return str(len(kmer_positions)) + kmer_positions
 
 
-def get_kmer_count_dict(kmer: str, file: Path) -> dict:
+def get_kmer_count_dict(
+    kmer: str, current_file: Path, next_file: Path, intervals_number: int, seqlen: int
+) -> dict:
     """Build the dict with the num of occurence of the given kmer as key and the lists
        of the correspondings reads as value
 
     Args:
         kmer (str): the given kmer
-        file (Path): the input file path
+        file (Path): the infile file path
 
     Returns:
         dict: dict with num of occurence as keys and reads lists as values
     """
+    not_sorted: list = []
     kmer_count_dict: dict = {}
-    reader: Generator = fasta_reader(file)
-    for read in reader:
-        kmer_count: str = str(get_sequence(read).count(kmer))
-        if not kmer_count in kmer_count_dict:
-            kmer_count_dict[kmer_count] = [read]
-        else:
-            kmer_count_dict[kmer_count].append(read)
+    reader: Generator = fasta_reader(current_file)
+    intervals = [i * (seqlen / intervals_number) for i in range(intervals_number)]
+    with open_wipe_and_add(next_file) as file:
+        for read in reader:
 
-    return kmer_count_dict
+            identifier: str = get_identifier(read=read, kmer=kmer, intervals=intervals)
+            if identifier == "0":
+                not_sorted.append(read)
+                file.write(read + "\n")
+            else:
+                if not identifier in kmer_count_dict:
+                    kmer_count_dict[identifier] = [read]
+                else:
+                    kmer_count_dict[identifier].append(read)
+
+    return kmer_count_dict, not_sorted
 
 
-def get_kmer_dict(kmer: str, input: Path, size: int, method: str) -> dict:
+def get_kmer_dict(
+    kmer: str,
+    infile: Path,
+    original_size: int,
+    intervals_number: int,
+    seqlen: int,
+    cutoff: int,
+) -> dict:
 
     kmer_dict: dict = {}
-    round_limit: int = 100
     breaking: bool = False
-
+    size = original_size
+    current_file = infile
+    next_file = "tempfile/kmer_sort_no_init"
     # a dict is build for the first kmer and then we pick another kmer to sort the
     # reads in which the previous kmer had 0 occurences
-    for _ in range(round_limit):
+
+    for round in range(cutoff):
 
         # build the occurence dict for the current kmer
-        kmer_dict[kmer]: dict = get_kmer_count_dict(kmer, input)
+        kmer_dict[kmer], not_sorted = get_kmer_count_dict(
+            kmer=kmer,
+            current_file=current_file,
+            next_file=next_file,
+            intervals_number=intervals_number,
+            seqlen=seqlen,
+        )
+        if current_file != infile:
+            os.remove(current_file)
+
+        current_file = next_file
+        next_file = current_file.split("no")[0] + f"no{str(round)}"
 
         # if all the reads has been sorted we end the loop
-        if not "0" in kmer_dict[kmer]:
+        if not_sorted == []:
             breaking: bool = True
             break
 
         else:
-            with open_wipe_and_add("method_2_temp") as file:
-                for read in kmer_dict[kmer]["0"]:
-                    file.write(read + "\n")
+            last_zero: list = deepcopy(not_sorted)
 
-            last_zero: list = kmer_dict[kmer]["0"]
-            del kmer_dict[kmer]["0"]
-
-        input: Path = "method_2_temp"
-        kmer: str = get_next_kmer(method=method, size=size, input=input)
+        kmer: str = get_random_kmer(size=size, infile=current_file)
+        current_size = size
+        while kmer in kmer_dict:
+            kmer: str = get_random_kmer(size=size, infile=current_file)
+            size += 1
+        size = current_size
 
     if not breaking:
-        # if we reach 100 kmer we keep the rest of the reads as an unsorted list
+        # if we reach 1000 kmer we keep the rest of the reads as an unsorted list
+        print("reaching 1000")
         kmer_dict["LAST"]: dict = {"0": last_zero}
+
     return kmer_dict
 
 
 @timer_func
-def sort_by_kmer(input: Path, output: Path, size: int, method: str) -> None:
-    """sort_by_kmer the read of the input file in the output file without the labels
+def sort_by_kmer(
+    infile: Path, outfile: Path, size: int, intervals_number: int, cutoff: int
+) -> None:
+    """sort_by_kmer the read of the infile file in the outfile file without the labels
 
     Args:
-        input (str): input file
-        output (str): output file
-        kmer (str, optional): first kmer to use may be random. Defaults to "".
+        infile (str): infile file
+        outfile (str): outfile file
         size (str, optional): if first kmer is random set size of kmer to use. Defaults to "".
     """
     random.seed(42)
-    first_kmer: str = get_next_kmer(method=method, size=size, input=input)
-
+    seqlen = len(get_sequence(next(fasta_reader(infile))))
+    first_kmer: str = get_random_kmer(size=size, infile=infile)
     kmer_dict: dict = get_kmer_dict(
-        method=method, kmer=first_kmer, input=input, size=size
+        kmer=first_kmer,
+        infile=infile,
+        original_size=size,
+        intervals_number=intervals_number,
+        seqlen=seqlen,
+        cutoff=cutoff,
     )
 
-    kmer_list: list = [kmer for kmer in kmer_dict.keys()]
-    kmer_list.sort()
-    sorted_kmer_dict: dict = {}
-
-    for kmer in kmer_list:
-        sorted_kmer_dict[kmer] = kmer_dict[kmer]
-
-    with open_wipe_and_add(output) as file:
-        for kmer in sorted_kmer_dict:
-            for count in sorted_kmer_dict[kmer]:
-                for read in sorted_kmer_dict[kmer][count]:
+    with open_wipe_and_add(outfile) as file:
+        for kmer in kmer_dict:
+            for count in kmer_dict[kmer]:
+                for read in kmer_dict[kmer][count]:
                     file.write(get_sequence(read) + "\n")
 
 
-def erro_handling(input: Path, size: int) -> None:
+def erro_handling(infile: Path, size: int) -> None:
     """Handle error regarding the kmer or size given in arguments
 
     Args:
-        input (Path): input file
+        infile (Path): infile file
         kmer (str): given kmer
         size (int): given size
 
@@ -169,7 +247,7 @@ def erro_handling(input: Path, size: int) -> None:
         ValueError: positive and non-null size check
         ValueError: size limit check
     """
-    reader: Generator = fasta_reader(filename=input)
+    reader: Generator = fasta_reader(filename=infile)
     read = next(reader)
 
     if size <= 0:
@@ -181,3 +259,16 @@ def erro_handling(input: Path, size: int) -> None:
 
 if __name__ == "__main__":
     pass
+
+# python read_sort.py -i data/ecoli_100Kb_reads_120x.fasta -m kmer_sort -s 8 -cu 3 -c data/headerless/ecoli_100Kb_reads_120x.fasta.headerless.gz -d True
+# python read_sort.py -i data/humch1_1Mb_reads_120x.fasta -m kmer_sort -s 8 -cu 3 -c data/headerless/humch1_1Mb_reads_120x_headerless.fasta.gz -d True
+#
+# 2 : 1.13
+# 3 : 1.16
+# 4 : 1.23
+#
+# 6 : 1.78
+# 6 cs 240_000 : 1.72
+# 6 cs 240_000 + ps : 1.72
+# 6 cs 300_000 : 1.72
+# 7 : 1.62
--- read_sort.py
+++ read_sort.py
@@ -9,9 +9,9 @@ def argparser():
     Parses arguments from command line
             Parameters:
                     Console arguments
-                    -i / --input (str) : Input fasta file with headers
+                    -i / --infile (str) : infile fasta file with headers
                     -m / --method (str): Method used to sort reads. Can be 'tsne_sort', 'pca_sort' or 'kmer_sort'
-                    -d / --delete_output (bool) : Delete output file after compression ratio is computed
+                    -d / --delete_output (bool) : Delete outfile file after compression ratio is computed
                     -c / --compare_to (str) : headerless fasta.gz file to compare to in order to compute compression ratio
                     -s / --size_kmer (int) : size of kmer used for kmer_sort
                     -cs / --chunk_size (int) : size of chunk used for tsne_sort and pca_sort
@@ -19,43 +19,45 @@ def argparser():
     parser = ArgumentParser()
 
     # Add the arguments to the parser
-    parser.add_argument("-i", "--input", required=False, help="")
+    parser.add_argument("-i", "--infile", required=False, help="")
     parser.add_argument("-d", "--delete_output", default=False, help="")
     parser.add_argument("-m", "--method", required=True, help="")
     parser.add_argument("-c", "--compare_to")
     parser.add_argument("-s", "--size_kmer", default=6, help="")
     parser.add_argument("-cs", "--chunk_size", default=1000000, help="")
+    parser.add_argument("-cu", "--cut", default=2, help="")
 
     args = parser.parse_args()
     return args
 
 
 def read_sort_main(
-    input: Path,
+    infile: Path,
     compare_to: Path,
     delete: bool = True,
     method: str = "kmer_sort",
     size: int = 6,
     chunk_size: int = 1000000,
+    cut: int = 2,
 ):
     log = {}
-    output = "_out.".join(input.split("."))
+    outfile = "_out.".join(infile.split("."))
 
     t1 = time.time()
 
     if method == "kmer_sort":
-        kmer_sort.sort_by_kmer(input=input, output=output, size=size, method="random")
-    if method == "tsne_sort":
-        tsne_sort.sort_by_tsne(input, output, int(chunk_size))
-    if method == "pca_sort":
-        pca_sort.sort_by_pca(input, output, int(chunk_size))
+        kmer_sort.sort_by_kmer(infile=infile, outfile=outfile, size=size, cut=cut)
+    elif method == "tsne_sort":
+        tsne_sort.sort_by_tsne(infile, outfile, int(chunk_size))
+    elif method == "pca_sort":
+        pca_sort.sort_by_pca(infile, outfile, int(chunk_size))
 
     log["time"] = time.time() - t1
-    log["rate"] = monitor_gzip(output, compare_to)
+    log["rate"] = monitor_gzip(outfile, compare_to)
 
     if delete:
-        os.remove(output)
-        os.remove(output + ".gz")
+        os.remove(outfile)
+        os.remove(outfile + ".gz")
 
     return log
 
@@ -63,25 +65,24 @@ def read_sort_main(
 def main():
     args = argparser()
 
-    output = "_out.".join(args.input.split("."))
+    outfile = "_out.".join(args.infile.split("."))
 
     if args.method == "kmer_sort":
         size = int(args.size_kmer)
-        kmer_sort.sort_by_kmer(
-            input=args.input, output=output, size=size, method="random"
-        )
+        cut = int(args.cut)
+        kmer_sort.sort_by_kmer(infile=args.infile, outfile=outfile, size=size, cut=cut)
 
-    if args.method == "tsne_sort":
-        tsne_sort.sort_by_tsne(args.input, output, int(args.chunk_size))
+    elif args.method == "tsne_sort":
+        tsne_sort.sort_by_tsne(args.infile, outfile, int(args.chunk_size))
 
-    if args.method == "pca_sort":
-        pca_sort.sort_by_pca(args.input, output, int(args.chunk_size))
+    elif args.method == "pca_sort":
+        pca_sort.sort_by_pca(args.infile, outfile, int(args.chunk_size))
 
-    print(f"Compression ratio : {monitor_gzip(output, args.compare_to)}")
+    print(f"Compression ratio : {monitor_gzip(outfile, args.compare_to)}")
 
-    if args.delete_output:
-        os.remove(output)
-        os.remove(output + ".gz")
+    if args.delete_output == "True":
+        os.remove(outfile)
+        os.remove(outfile + ".gz")
 
 
 if __name__ == "__main__":
