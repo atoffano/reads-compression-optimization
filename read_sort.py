2d87076f3dc6b9c49b7b611ef918c341699f46f6
atoffano <antoine.toffano@gmail.com>
2022-12-11 15:00:20 +0100

    readsort call fix

--- read_sort.py
+++ read_sort.py
@@ -88,14 +88,14 @@ def main():
         )
 
     if args.method == "tsne_sort":
-        tsne_sort.sort_by_tsne(args.input, output, int(args.chunk_size))
+        tsne_sort.sort_by_tsne(args.infile, output, int(args.chunk_size))
 
     if args.method == "pca_sort":
-        pca_sort.sort_by_pca(args.input, output, int(args.chunk_size))
+        pca_sort.sort_by_pca(args.infile, output, int(args.chunk_size))
 
     if args.method == "chatgpt_sort":
         chatGPT_sort.sort_by_minimizer(
-            args.input, output
+            args.infile, output
         )
 
     print(f"Compression ratio : {monitor_gzip(output, args.compare_to)}")
