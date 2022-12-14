f50ea83c774635958de32b7c77d8b0ec8ee39ef4
atoffano <antoine.toffano@gmail.com>
2022-12-14 19:26:26 +0100

    rollinghash docstring

--- rollinghash_sort.py
+++ rollinghash_sort.py
@@ -1,15 +1,18 @@
-import numpy as np
-import random
 from utils import *
-import os
 
 @timer_func
 def sort_by_minimizer(infile, outfile):
+    """A function to sort a .fasta file with the rolling hash method, proposed by chatGPT.
+
+    Args:
+        infile (str): Input .fasta file
+        outfile (str): Output .fasta file without headers
+    """
     with open(infile, 'r') as file:
-        reads = [line.strip() for line in file.readlines() if line[0] != '>']
+        reads = [line.strip() for line in file.readlines() if line[0] != '>'] # Remove headers if leftovers
 
     # define the length of the minimizer and the number of mismatches allowed
-    MINIMIZER_LEN = 8
+    MINIMIZER_LEN = 8 # Size of both minimizer and of rolling window
     MAX_MISMATCHES = 1
 
     # compute the minimizers for each read
@@ -19,7 +22,7 @@ def sort_by_minimizer(infile, outfile):
         rolling_hash = 0
         window = read[:MINIMIZER_LEN]
         for char in window:
-            rolling_hash = rolling_hash * 31 + ord(char)
+            rolling_hash = rolling_hash * 31 + ord(char) # ord() returns the unicode code point for a one-character string
         minimizer = rolling_hash
 
         # iterate over the remaining characters in the read
@@ -32,64 +35,16 @@ def sort_by_minimizer(infile, outfile):
             if rolling_hash < minimizer:
                 minimizer = rolling_hash
         minimizers.append(minimizer)    # Sort dictionary by minimizers and generate new .fasta file with reads in new order
-    sorted_reads = [x for _, x in sorted(zip(minimizers, reads))]
+    sorted_reads = [x for _, x in sorted(zip(minimizers, reads))] # Sorts the reads according to the minimizers
 
     # write the reordered reads to a new .fasta file
     with open(outfile, 'a') as handle:
         for read in sorted_reads:
             handle.write(read + "\n")
 
-
-@timer_func
-def sort_by_minimizer2(infile, outfile):
-    with open(infile, 'r') as file:
-        reads = [line.strip() for line in file.readlines() if line[0] != '>']
-    
-        # Create a dictionary to store the minimizers and their corresponding sequences
-        min_dict = {}
-        
-        # Iterate through the reads and find the minimizer for each sequence
-        for read in reads:
-            # Get the minimizer for the current sequence
-            min_seq = minimizer(read)
-            
-            # Add the minimizer and corresponding sequence to the dictionary
-            min_dict[min_seq] = read
-        
-        # Open a new .fasta file to write the reordered sequences
-        with open(outfile, 'a') as f_out:
-            # Sort the dictionary keys (minimizers) in ascending order
-            sorted_keys = sorted(min_dict.keys())
-            
-            # Iterate through the sorted keys and write the corresponding sequences to the new .fasta file
-            for key in sorted_keys:
-                f_out.write(min_dict[key] + '\n')
-
-def minimizer(sequence):
-    # Set the k-mer length and window size
-    k = 4
-    w = 10
-
-    # Initialize the minimizer to be the first k-mer in the sequence
-    min_seq = sequence[:k]
-
-    # Iterate through the sequence with the specified window size
-    for i in range(len(sequence) - w):
-        # Get the current window of the sequence
-        window = sequence[i:i+w]
-        
-        # Iterate through the window and find the minimum k-mer
-        for j in range(len(window) - k):
-            curr_seq = window[j:j+k]
-            if curr_seq < min_seq:
-                min_seq = curr_seq
-
-    # Return the minimizer sequence
-    return min_seq
-
-
 if __name__ == '__main__':
+    import os
     sort_by_minimizer('data/ecoli_100Kb_reads_40x.fasta', 'out_x.fasta')
     print(monitor_gzip("out_x.fasta", 'data/headerless/ecoli_100Kb_reads_40x.fasta.headerless.gz'))
-    # os.remove("out_x.fasta")
-    # os.remove("out_x.fasta.gz")
+    os.remove("out_x.fasta")
+    os.remove("out_x.fasta.gz")
