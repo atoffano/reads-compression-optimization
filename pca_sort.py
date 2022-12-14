ab2565063e678c0d77a32996549f8be5da579609
atoffano <antoine.toffano@gmail.com>
2022-12-14 19:06:47 +0100

    docstring pca_sort

--- pca_sort.py
+++ pca_sort.py
@@ -14,7 +14,7 @@ def enum_kmers(k):
         k (int): [description]
 
     Returns:
-        list (list): A lit of all possible kmers of length k.
+        list (list): A list of all possible kmers of length k.
     """
     if k<0:
         raise ValueError()
@@ -29,7 +29,14 @@ def enum_kmers(k):
 
 
 def distribution(list):
-    """Distribute the kmers in the list."""
+    """Distribute the kmers in the list.
+    
+    Args:
+        list (list): A list of kmers.
+
+    Returns:
+        list (list): A list of kmers with all possible kmers of length k.
+    """
     adn = ['A','T','C','G']
     res = []
     i = 0
@@ -44,34 +51,64 @@ def distribution(list):
 
 
 def encode_kmers(kmers, sequence):
+    """Encode the kmers in the sequence.
+        
+    Args:
+        kmers (list): List of all kmers of length k.
+        sequence (str): Sequence to encode.
+
+    Returns:
+        list (list): A list of 1s and 0s, 1 if the kmer is in the sequence, 0 otherwise.
+    """
     return [1 if kmer in sequence else 0 for kmer in kmers]
 
 
 def sort_by_pca(infile: Path, outfile: Path, chunk_size):
+    """Sort the reads in the input file by PCA.
+
+    Args:
+        infile (str): Path to the input file.
+        outfile (str): Path to the output file.
+        chunk_size (int): Number of reads to compute at once using PCA. Useful if the file can't hold in memory.
+    """
     # kmer embedding sorting
-    kmers = enum_kmers(3)
+    kmers = enum_kmers(3) # Compute all kmers of length 3
     i = 0
     data = []
     chunk_size = chunk_size if chunk_size != 0 else len(list(fasta_reader(infile)))
     matrix = np.zeros((chunk_size, len(kmers)), dtype=int)
     for read in fasta_reader(infile):
         sequence = get_sequence(read)
-        matrix[i] = encode_kmers(kmers, sequence)
+        matrix[i] = encode_kmers(kmers, sequence) # Encode the sequence
         i += 1
         data.append(sequence)
         if i == chunk_size:
-            i, data, matrix = pc_sort(i, matrix, data, chunk_size, kmers, outfile)
+            i, data, matrix = pc_sort(i, matrix, data, chunk_size, kmers, outfile) # Calls the function to compute the PCA on reaching chunk size.
     if i > 0:
-        i, data, matrix = pc_sort(i, matrix, data, chunk_size, kmers, outfile)
+        i, data, matrix = pc_sort(i, matrix, data, chunk_size, kmers, outfile) # Calls the function to compute the PCA on reaching the end of the file.
 
 
 @timer_func
 def pc_sort(i, matrix, data, chunk_size, kmers, outfile):
-    pca = PCA(n_components=1)
-    PC = pca.fit_transform(matrix).flatten()
-    for pc, seq, j in zip(PC, data, range(chunk_size)):
+    """Compute the PCA on the current block of reads, sort and write them to output.
+
+    Args:
+        i (int): Number of reads in the current block.
+        matrix (np.array): Matrix of encoded reads.
+        data (list): List of reads.
+        chunk_size (int): Number of reads to compute at once using PCA. Useful if the file can't hold in memory.
+        kmers (list): List of all kmers of length k.
+        outfile (str): Path to the output file.
+
+    Returns:
+        i, data, matrix (tuple): Tuple of the number of reads in the current block, the list of reads and the matrix of encoded reads.
+
+    """
+    pca = PCA(n_components=1) # Initialize PCA with 1 component only
+    PC = pca.fit_transform(matrix).flatten() # Compute the first component of the PCA
+    for pc, seq, j in zip(PC, data, range(chunk_size)): # Align a read with its first component of the PCA
         data[j] = (pc, seq)
-    data = sorted(data, key=lambda x: x[0])
+    data = sorted(data, key=lambda x: x[0]) # Sort the reads by the first component of the PCA
     with open(outfile, "a") as f:
         for couple in data:
             f.write(couple[1] + "\n")
